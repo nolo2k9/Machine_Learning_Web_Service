7c57e141fcdcd58087c6df845595d98666370c98
Keith Nolan <g00351932@gmit.ie>
2020-12-28 19:12:02 +0000

    started the web service

--- web-service.py
+++ web-service.py
@@ -1,9 +1,29 @@
 
 # adapted from: https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application
 
-from flask import Flask
+from flask import Flask, request, jsonify # loading in Flask
+#from ludwig.api import LudwigModel # loading in Ludwig
+import pandas as pd # loading pandas for reading csv
+from tensorflow.keras.models import load_model
+
+# creating a Flask application
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Load the model
+model = load_model("model.h5")
+# creating predict url and only allowing post requests.
+@app.route('/pred', methods=['POST'])
+def predict():
+    # Get data from Post request
+    data = request.get_json()
+    # Make prediction
+    df = pd.DataFrame([str(data['text'])], columns=['content'])
+    print(df.head())
+    # making predictions
+    pred = model.predict(dataset=df, data_format='df')
+    print(pred)
+    # returning the predictions as json
+    return jsonify(pred['airline_sentiment_predictions'][0])
+
+if __name__ == '__main__':
+    app.run(port=3020, debug=True)
